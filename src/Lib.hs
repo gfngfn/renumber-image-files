c57768d5ae0136759d16497170b259d0a73e7f8c
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2018-12-03 04:41:01 +0900

    implement simplified 'ls' command

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
+    ( getFileList
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified System.Directory as Dir
+
+getFileList :: IO ()
+getFileList =
+  Dir.getDirectoryContents "." >>= printList
+
+printList :: [String] -> IO ()
+printList [] = return ()
+printList (s : ss) = putStrLn s >>= \() -> printList ss
