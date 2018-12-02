4b24ac91dbefcaf796a842988b6eb485789e6a3a
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2018-12-03 06:03:55 +0900

    begin to use attoparsec

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( getFileList
     ) where
 
 import qualified System.Directory as Dir
+import qualified Data.Text as T
+import Data.Attoparsec.Text
 
 getFileList :: IO ()
 getFileList =
@@ -10,4 +14,19 @@ getFileList =
 
 printList :: [String] -> IO ()
 printList [] = return ()
-printList (s : ss) = putStrLn s >>= \() -> printList ss
+printList (s : ss) =
+  case s of
+    '.' : _ -> putStrLn ("* " ++ s) >>= \() -> printList ss
+    _       -> putStrLn ("  " ++ s) >>= \() -> printList ss
+
+{-
+fileNameParser :: Parser (Text, Int)
+fileNameParser = do
+  s <- word
+-}
+
+word :: Parser String
+word = do
+  c <- letter
+  s <- word
+  return (c : s)
