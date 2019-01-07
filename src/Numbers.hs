4282ec8920cdb14b3c2ce4276482c99e0af35fe7
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2019-01-07 11:29:31 +0900

    rename 'J' with 'Json'

--- src/Numbers.hs
+++ src/Numbers.hs
@@ -3,15 +3,15 @@ module Numbers where
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.Aeson as J
+import qualified Data.Aeson as Json
 import qualified Data.Scientific as Sci
 
 
 type Numbers = HashMap.HashMap T.Text Int
 
 
-decodeInt :: J.Value -> Int
-decodeInt (J.Number num) =
+decodeInt :: Json.Value -> Int
+decodeInt (Json.Number num) =
   case (Sci.floatingOrInteger num :: Either Double Int) of
     Right n -> n
     Left x -> round x
@@ -20,10 +20,10 @@ decodeInt (J.Number num) =
 load :: FilePath -> IO (Maybe Numbers)
 load path = do
   s <- BS.readFile path
-  let maybejson = J.decode s :: Maybe J.Value
+  let maybejson = Json.decode s :: Maybe Json.Value
   case maybejson of
     Nothing ->
       return Nothing
 
-    Just (J.Object hashmap) ->
+    Just (Json.Object hashmap) ->
       return (Just (HashMap.map decodeInt hashmap))
