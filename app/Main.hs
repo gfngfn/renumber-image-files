77a1b3183151b65e9cb7538a9197070daed6f461
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-03-29 18:15:12 +0900

    create 'LibIO'

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
-import qualified System.Directory as Dir
-
 import qualified Lib
+import qualified LibIO
 import qualified TagMap
+
 
 main :: IO ()
 main = do
-  fnames <- Dir.listDirectory "."
+  fnames <- LibIO.listDirectory "."
   let (errsParse, res) = Lib.checkFileList fnames
   case res of
     Left errsDup -> do
-      mapM_ Lib.printError (errsParse ++ errsDup)
+      mapM_ LibIO.printError (errsParse ++ errsDup)
       putStrLn "Conflicts of numbers should be fixed manually."
 
     Right tagMap -> do
-      mapM_ Lib.printError errsParse
+      mapM_ LibIO.printError errsParse
       let (renumInfos, _numNextMap) = TagMap.getRenumberInfos tagMap
-      putStrLn "Success."
-      mapM_ Lib.printRenumberInfo renumInfos
+      putStrLn "No conflicts found."
+      mapM_ LibIO.printRenumberInfo renumInfos
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,6 @@ module Lib where
 import qualified Data.Ord
 import qualified Data.List as List
 import qualified Data.Either as Either
-import qualified Data.Map.Strict as Map
-import Control.Monad
 
 import Types
 import qualified FileNameParser
@@ -38,76 +36,3 @@ makeValidationMap files =
         Left err        -> (err : errAcc, tagMap)
   in
   List.foldl validateSingle ([], TagMap.empty) files
-
-
-printError :: Error -> IO ()
-printError err =
-  case err of
-    CannotParseFileName fname ->
-      putStrLn $ "! Cannot parse '" ++ fname ++ "'"
-
-    SingleAlreadyExists tag n ext1 i ext2 ->
-      let
-        fname1 = showFile tag n Nothing ext1
-        fname2 = showFile tag n (Just i) ext2
-      in
-      mapM_ putStrLn
-        [ "! There already exists a single:"
-        , "  * " ++ fname1
-        , "  and thus cannot validate:"
-        , "  * " ++ fname2
-        ]
-
-    MultipleAlreadyExists tag n multMap ext2 ->
-      let
-        fname2 = showFile tag n Nothing ext2
-      in do
-        putStrLn $ "! There already exists a multiple:"
-        printMultiple tag n multMap
-        putStrLn $ "  and should rename:"
-        putStrLn $ "  * " ++ fname2
-
-    DuplicatedSingle tag n ext1 ext2 ->
-      let
-        fname1 = showFile tag n Nothing ext1
-        fname2 = showFile tag n Nothing ext2
-      in
-      mapM_ putStrLn
-        [ "! Duplication as to extension:"
-        , "  * " ++ fname1
-        , "  * " ++ fname2
-        ]
-
-    DuplicatedMultiple tag n i ext1 ext2 ->
-      let
-        fname1 = showFile tag n (Just i) ext1
-        fname2 = showFile tag n (Just i) ext2
-      in
-      mapM_ putStrLn
-        [ "! Duplication as to extension:"
-        , "  * " ++ fname1
-        , "  * " ++ fname2
-        ]
-
-
-printMultiple :: Tag -> Number -> Map.Map Index Extension -> IO ()
-printMultiple tag n multMap =
-  forM_ (Map.toList multMap)
-    (\(i, ext) -> putStrLn $ "  * " ++ showFile tag n (Just i) ext)
-
-
-showFile :: Tag -> Number -> Maybe Index -> Extension -> String
-showFile tag n iopt ext =
-  let
-    istr =
-      case iopt of
-        Nothing -> ""
-        Just i  -> ":" ++ show i
-  in
-  tag ++ "[" ++ show n ++ istr ++ "]." ++ ext
-
-
-printRenumberInfo :: RenumberInfo -> IO ()
-printRenumberInfo renumInfo =
-  let RenumberInfo (FileInfo (tag, numOld, iopt, ext), numNew) = renumInfo in
-  putStrLn $ showFile tag numOld iopt ext ++ " ---> " ++ show numNew
