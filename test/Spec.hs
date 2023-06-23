bf581c0bf830d2c02864e0157b6037c40c3711fc
Takashi Suwa <bd.gfngfn@gmail.com>
2023-06-23 16:53:55 +0900

    introduce HUnit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  _ <- runTestTT test1
+  putStrLn "finished."
+
+test1 =
+  TestCase (assertEqual "small test for addition" 3 (1 + 2))
